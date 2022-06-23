efc3a3ac0f2cc65289059914216a0a7b471e96e0
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-24 00:19:47 +0200

    Backup

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,11 +10,11 @@
     <title>Document</title>
 </head>
 <body>
-@include('partials.header');
+@include('partials.header')
 
-@yield('content');
+@yield('content')
 
-@include('partials.footer');
+@include('partials.footer')
 
 
 
